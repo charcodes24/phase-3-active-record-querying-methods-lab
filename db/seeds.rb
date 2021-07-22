c60a1e4e529ab189fd7c1bc3c192e6c4ee36fe1e
charcodes24 <charcodes24@gmail.com>
2021-07-22 07:46:32 -0700

    commit tree 3c0e8e90877f88383b4785a38267b4bc5e0b5429

---
+++ db/seeds.rb
@@ -0,0 +1,56 @@
+Show.create(
+    name: "Forest Gump", 
+    network: "NBC",
+    day: "Tuesday",
+    rating: 5
+);
+
+Show.create(
+    name: "Jeopardy", 
+    network: "ABC",
+    day: "Friday",
+    rating: 5
+);
+
+Show.create(
+    name: "WoF", 
+    network: "ABC",
+    day: "Wednesday",
+    rating: 3
+);
+
+Show.create(
+    name: "90 day fiance", 
+    network: "TLC",
+    day: "Saturday",
+    rating: 1
+);
+
+Show.create(
+    name: "Bachelorette", 
+    network: "Fox",
+    day: "Thursday",
+    rating: 0
+);
+
+Show.create(
+    name: "American Ninja Warrior ", 
+    network: "CNN",
+    day: "Monday",
+    rating:2
+);
+
+Show.create(
+    name: "Ridiculousnous", 
+    network: "MTV",
+    day: "Friday",
+    rating: 4
+);
+
+
+
+
+
+
+
+
